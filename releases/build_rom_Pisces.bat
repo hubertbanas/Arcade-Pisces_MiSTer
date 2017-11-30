e15478096f3a9e16113ff5cb1965119047bfcf52
sorgelig <pour.garbage@gmail.com>
2017-12-01 07:35:55 +0800

    Initial implementation.

---
+++ releases/build_rom_Pisces.bat
@@ -0,0 +1,40 @@
+@echo off
+
+set    zip=pisces.zip
+set ifiles=p1.bin+p2.bin+p3.bin+p4.bin+p5.bin+p6.bin+p6.bin+p6.bin+g09.bin+g11.bin+g10.bin+g12.bin
+set  ofile=a.pisces.rom
+
+rem =====================================
+setlocal ENABLEDELAYEDEXPANSION
+
+set pwd=%~dp0
+echo.
+echo.
+
+if EXIST %zip% (
+
+	!pwd!7za x -otmp %zip%
+	if !ERRORLEVEL! EQU 0 ( 
+		cd tmp
+
+		copy /b/y %ifiles% !pwd!%ofile%
+		if !ERRORLEVEL! EQU 0 ( 
+			echo.
+			echo ** done **
+			echo.
+			echo Copy "%ofile%" into root of SD card
+		)
+		cd !pwd!
+		rmdir /s /q tmp
+	)
+
+) else (
+
+	echo Error: Cannot find "%zip%" file
+	echo.
+	echo Put "%zip%", "7za.exe" and "%~nx0" into the same directory
+)
+
+echo.
+echo.
+pause
